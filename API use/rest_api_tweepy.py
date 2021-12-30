f08ac66efa1104af49e2efb2af3d1451b4e8e618
YooInBeom <yib950@gmail.com>
2021-12-30 17:06:37 +0900

    Add tweepy

---
+++ API use/rest_api_tweepy.py
@@ -0,0 +1,16 @@
+import os
+import tweepy
+
+CONSUMER_KEY = os.environ['CONSUMER_KEY']
+CONSUMER_SECRET = os.environ['CONSUMER_SECRET']
+ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
+ACCESS_TOKEN_SECRET = os.environ['ACCESS_TOKEN_SECRET']
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+api = tweepy.API(auth)
+public_tweets = api.home_timeline()
+
+for status in public_tweets:
+    print('@' + status.user.screen_name, status.text)
